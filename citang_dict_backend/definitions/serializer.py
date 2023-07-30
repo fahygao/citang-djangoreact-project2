52b2095e1cdbdfc819d2ada7d14a53b904f7e1a5
fahygao <fahygao@gmail.com>
2023-07-29 23:51:23 -0400

    change files

--- citang_dict_backend/definitions/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import *
-
-
-class DefinitionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Definition
-        fields = ['word','long_Definition']
