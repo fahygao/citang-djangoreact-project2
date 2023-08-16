9eff1cdee368173d3305c1e383f96c4e162c0982
fahygao <fahygao@gmail.com>
2023-08-16 02:15:02 -0400

    add search bar

--- src/Search.js
+++ src/Search.js
@@ -34,14 +34,14 @@ export default function Search({details}) {
       return (
         <section>
             <h2 className="searchheader">搜搜看你想了解的梗词吧！</h2>
-          <p className="pa2 bb br3">
+          <div  className="pa2 bb br3">
             <input 
               className="pa3 bb br3 grow b--none bg-lightest-white ma3"
               type = "search" 
               placeholder = "查找梗词" 
               onChange = {handleChange}
             />
-          </p>
+          </div >
           {searchList()}
         </section>
       );
